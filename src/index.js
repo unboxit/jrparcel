f61c562d6964bcb049e716bb6bae03a5fcf8de2e
Gabriel Dumitrescu <gdumitrescu@users.noreply.github.com>
2018-04-06 04:21:23 -0400

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React, {Component} from 'react';
+import { render } from 'react-dom';
+import App from './modules/App';
+import './index.css';
+
+render(<App />, document.getElementById('root'));
